3ea657621c04df55fd66f848d64348fd299e2d1a
Jinhyun Kim <jinkim.dev@gmail.com>
2021-06-01 15:23:48 -0700

    Update DI with NotificationScheduler, retrieve it in App class to schedule periodic work.

--- app/src/main/java/dev/jinkim/android/helloword/HelloWordApp.kt
+++ app/src/main/java/dev/jinkim/android/helloword/HelloWordApp.kt
@@ -3,14 +3,18 @@ package dev.jinkim.android.helloword
 import android.app.Application
 import dev.jinkim.android.helloword.injection.Modules.fragmentModule
 import dev.jinkim.android.helloword.injection.Modules.networkModule
+import dev.jinkim.android.helloword.injection.Modules.notificationSchedulerModule
 import dev.jinkim.android.helloword.injection.Modules.viewModelModule
 import dev.jinkim.android.helloword.injection.Modules.wordsRepositoryModule
 import dev.jinkim.android.helloword.notification.NotificationScheduler
 import dev.jinkim.android.helloword.notification.NotificationWorker
+import org.koin.android.ext.android.getKoin
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.workmanager.dsl.worker
 import org.koin.androidx.workmanager.koin.workManagerFactory
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import org.koin.dsl.module
 
 class HelloWordApp : Application() {
@@ -23,14 +27,15 @@ class HelloWordApp : Application() {
         }
 
         startKoin {
+            androidLogger(Level.DEBUG)
             androidContext(this@HelloWordApp)
             // Custom WorkManager init to use with Koin
             workManagerFactory()
-            modules(listOf(wordsRepositoryModule, networkModule, fragmentModule, viewModelModule, workerModule))
+            modules(listOf(wordsRepositoryModule, networkModule, notificationSchedulerModule, fragmentModule, viewModelModule, workerModule))
         }
 
-        // Schedule periodic work for notifications
-        val notificationScheduler = NotificationScheduler(this)
+        // Retrieve NotificationScheduler from Koin and schedule periodic work for notifications
+        val notificationScheduler = getKoin().get<NotificationScheduler>()
         notificationScheduler.schedulePeriodicWork()
     }
 
--- app/src/main/java/dev/jinkim/android/helloword/injection/Modules.kt
+++ app/src/main/java/dev/jinkim/android/helloword/injection/Modules.kt
@@ -2,14 +2,16 @@ package dev.jinkim.android.helloword.injection
 
 import dev.jinkim.android.helloword.data.WordsRepository
 import dev.jinkim.android.helloword.network.*
+import dev.jinkim.android.helloword.notification.NotificationScheduler
 import dev.jinkim.android.helloword.ui.main.MainFragment
 import dev.jinkim.android.helloword.ui.main.MainViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 object Modules {
 
     val wordsRepositoryModule = module {
-        factory { WordsRepository(get()) }
+        single { WordsRepository(get()) }
     }
 
     val networkModule = module {
@@ -20,6 +22,10 @@ object Modules {
         factory { provideWordApi(get()) }
         factory { provideLoggingInterceptor() }
 //    factory { ResponseHandler() }
+    }
+
+    val notificationSchedulerModule = module {
+        factory { NotificationScheduler(androidContext()) }
     }
 
     val fragmentModule = module {
